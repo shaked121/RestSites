bb34593879603e57d2277312c96663e936277b22
shaked121 <brondvainshaked@gmail.com>
2026-01-29 18:47:25 +0200

    added comments

--- Scripts/code.py
+++ Scripts/code.py
@@ -11,15 +11,19 @@ for line in file1:
     line = line.strip()
 
     if line.startswith(">"):
-        # analyze previous gene
+
+        # אם זה לא הגן הראשון - מנתחים את הגן הקודם
         if gene != "":
             DsaI = re.findall(r"CC[AG][CT]GG", gene)
             SecI = re.findall(r"CC[ACGT]{2}GG", gene)
             CjuI = re.findall(r"CA[CT][ACGT]{5}[AG]TG", gene)
-            if(len(DsaI) == 0 and len(SecI) == 0 and len(CjuI) == 0):
+
+            # אם לא נמצא אף אתר
+            if len(DsaI) == 0 and len(SecI) == 0 and len(CjuI) == 0:
                 file2.write("No restriction sites found.\n")
             else:
-                count += 1
+                count += 1   # חלבון שיש בו לפחות אתר אחד
+
                 file2.write("There are " + str(len(DsaI)) + " DsaI sites:\n")
                 for x in DsaI:
                     file2.write(x + "\n")
@@ -32,15 +36,46 @@ for line in file1:
                 for x in CjuI:
                     file2.write(x + "\n")
 
-
             file2.write("\n")
 
+        # שומרים header חדש ומאפסים את הרצף
         header = line
         file2.write(header + "\n")
-        gene = ""   # IMPORTANT reset
+        gene = ""
 
     else:
         gene += line
+
+
+# ================================
+# ניתוח הגן האחרון בקובץ !!!
+# ================================
+if gene != "":
+    DsaI = re.findall(r"CC[AG][CT]GG", gene)
+    SecI = re.findall(r"CC[ACGT]{2}GG", gene)
+    CjuI = re.findall(r"CA[CT][ACGT]{5}[AG]TG", gene)
+
+    if len(DsaI) == 0 and len(SecI) == 0 and len(CjuI) == 0:
+        file2.write("No restriction sites found.\n")
+    else:
+        count += 1
+
+        file2.write("There are " + str(len(DsaI)) + " DsaI sites:\n")
+        for x in DsaI:
+            file2.write(x + "\n")
+
+        file2.write("There are " + str(len(SecI)) + " SecI sites:\n")
+        for x in SecI:
+            file2.write(x + "\n")
+
+        file2.write("There are " + str(len(CjuI)) + " CjuI sites:\n")
+        for x in CjuI:
+            file2.write(x + "\n")
+
+    file2.write("\n")
+
+
 file2.write("Number of Protein with any kind of site: " + str(count) + "\n")
+
 file1.close()
 file2.close()
